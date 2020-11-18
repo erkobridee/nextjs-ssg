1a8a283ea3007f36e8a459bf5c2f6735e3957776
Erko Bridee <erko.bridee@gmail.com>
2020-11-18 14:33:37 +0100

    Updates

--- _next/static/wY1fVvzt9f509esgy2LCL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fbd692b7ec6e078017770.css","static\u002Fchunks\u002Fpages\u002Findex-6032ea6937f6a9e1928f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
