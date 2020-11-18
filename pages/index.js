af6987773286bf0f3449c4970bba5aaf040b8e8e
Erko Bridee <erko.bridee@gmail.com>
2020-11-18 15:26:19 +0100

    update: imports to use absolut paths

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
+
+import styles from 'styles/Home.module.scss';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
 
       <main className={styles.main}>
@@ -57,7 +58,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
