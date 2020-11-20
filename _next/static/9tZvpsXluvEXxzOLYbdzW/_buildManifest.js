e378cf5b63b06878233354d6d6ebb04df1482981
erkobridee <erkobridee@users.noreply.github.com>
2020-11-20 08:55:49 +0000

    deploy: 2ba4d9e47b11159820033e83aa102ba29fe85d1e

--- _next/static/9tZvpsXluvEXxzOLYbdzW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F5becfe314be58847e07c.css","static\u002Fchunks\u002Fpages\u002Findex-7961817dfeb6d48ee0fe.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/JIEL49wla8sOR1XS0ITRU/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fjsonplaceholder\u002Fusers","\u002Fjsonplaceholder\u002Fusers\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
