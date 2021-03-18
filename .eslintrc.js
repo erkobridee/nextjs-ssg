948bc6264c98d1b23616d87a9291a0abb2d0efe2
Erko Bridee <erko.bridee@gmail.com>
2021-03-18 10:57:02 +0100

    feat(project): eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2018,
+    // Allows for the use of imports
+    sourceType: 'module',
+  },
+  rules: {
+    // Disable prop-types as we use TypeScript for type checking
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    // needed for NextJS's jsx without react import
+    'react/react-in-jsx-scope': 'off',
+  },
+  globals: { React: 'writable' },
+};
