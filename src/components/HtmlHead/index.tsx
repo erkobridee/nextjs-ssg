f637c2d1edb2cf7b7101e171075fcde80adb2039
Erko Bridee <erko.bridee@gmail.com>
2021-03-18 09:00:18 +0100

    refactor(components): migrate HtmlHead to typescript

---
+++ src/components/HtmlHead/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import Head from 'next/head';
+
+export interface IHtmlHeadProps {
+  title?: string;
+}
+
+export const HtmlHead: React.FunctionComponent<IHtmlHeadProps> = ({
+  title,
+  children,
+}) => (
+  <Head>
+    <title>{title && `${title} | `}Hello Next.js SSG</title>
+    {children}
+  </Head>
+);
+
+export default HtmlHead;
