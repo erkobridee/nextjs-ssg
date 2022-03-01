3ec9ab81cab95f08d8be5607336f254ce9247e91
erkobridee <erko.bridee@gmail.com>
2022-03-01 23:39:20 +0100

    fix: prettier code changes

--- src/components/Layout/JsonPlaceholder/LayoutJsonPlaceholder.tsx
+++ src/components/Layout/JsonPlaceholder/LayoutJsonPlaceholder.tsx
@@ -11,17 +11,18 @@ export interface ILayoutJsonPlaceholderProps {
   headerClassName?: string;
 }
 
-export const LayoutJsonPlaceholder: React.FunctionComponent<ILayoutJsonPlaceholderProps> =
-  ({ children, className, containerClassName }) => (
-    <LayoutBase
-      {...{
-        children,
-        className,
-        containerClassName,
-        header: <TopNavJsonPlaceholder />,
-        htmlHead: <HtmlHead title="JSON Placeholder" />,
-      }}
-    />
-  );
+export const LayoutJsonPlaceholder: React.FunctionComponent<
+  ILayoutJsonPlaceholderProps
+> = ({ children, className, containerClassName }) => (
+  <LayoutBase
+    {...{
+      children,
+      className,
+      containerClassName,
+      header: <TopNavJsonPlaceholder />,
+      htmlHead: <HtmlHead title="JSON Placeholder" />,
+    }}
+  />
+);
 
 export default LayoutJsonPlaceholder;
