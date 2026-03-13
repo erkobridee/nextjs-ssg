3a0627be43f6baac6022823f87cab1809eac0f83
erkobridee <erko.bridee@gmail.com>
2026-03-13 21:00:27 +0100

    #708 update(project): configs

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+
+    // TODO: review and remove it later
+    'src/_OLD_pages/**'
+  ])
+]);
+
+export default eslintConfig;
---
+++ next.config.ts
@@ -0,0 +1,40 @@
+import type { NextConfig } from 'next';
+
+import { basePath, distDir } from './src/utils/settings';
+
+//----------------------------------------------------------------------------//
+
+const nextConfig: NextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: 'export',
+
+  /**
+   * Redefines the output folder
+   *
+   * @see https://nextjs.org/docs/app/api-reference/config/next-config-js/distDir
+   */
+  distDir,
+
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath,
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true
+  }
+};
+
+export default nextConfig;
