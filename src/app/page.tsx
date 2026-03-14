5442101bf4a57f4f4fa158e562dc2a5d974d8cc3
erkobridee <erko.bridee@gmail.com>
2026-03-14 15:22:21 +0100

    #708 update(home page): code and definitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 
 import redefineUrl from '~/utils/redefineUrl';
+import { isProduction } from '~/utils/settings';
+
+import hello from '@/public/hello.json';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
+      <main className="xs:items-start flex min-h-screen w-full max-w-3xl flex-col items-center justify-between gap-6 bg-white px-16 py-16 lg:py-32 dark:bg-black">
         <Image
           className="dark:invert"
           src={redefineUrl('/next.svg')}
@@ -15,31 +18,34 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
+          {!isProduction && (
+            <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
+              To get started, edit the page.tsx file.
+            </h1>
+          )}
+
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{' '}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
+            <a href="https://vercel.com/templates" className="font-medium text-zinc-950 dark:text-zinc-50">
               Templates
             </a>{' '}
             or the{' '}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
+            <a href="https://nextjs.org/learn" className="font-medium text-zinc-950 dark:text-zinc-50">
               Learning
             </a>{' '}
             center.
           </p>
         </div>
+
+        <div>
+          <code className="rounded-[5px] bg-gray-100 p-1 font-mono text-[1.1rem] text-zinc-950">hello.json</code> -{' '}
+          {hello.message}
+        </div>
+
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] md:w-39.5 dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://vercel.com/new"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -54,7 +60,7 @@ export default function Home() {
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 md:w-39.5 dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://nextjs.org/docs"
             target="_blank"
             rel="noopener noreferrer"
           >
