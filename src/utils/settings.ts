d30d3f0112ca6a1cb9560aeeb94a9fe2ff34d4ec
erkobridee <erko.bridee@gmail.com>
2026-04-01 21:35:50 +0200

    #708 feat(settings utils): add support to github pages build

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -10,6 +10,8 @@ const { name } = packageJSON;
 
 export const isProduction = 'production' === `${process.env.NODE_ENV}`;
 
+export const isGHPage = 'true' === `${process.env.GH_PAGE}`;
+
 export const basePath = isProduction ? `/${name}` : '';
 
-export const distDir = isProduction ? `out/${name}` : '.next';
+export const distDir = isProduction ? (isGHPage ? `out` : `out/${name}`) : '.next';
