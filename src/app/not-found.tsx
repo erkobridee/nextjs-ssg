c593bb6df471db6f217c1da9f517448098562af8
erkobridee <erko.bridee@gmail.com>
2026-04-03 14:54:14 +0200

    #708 feat(not found page): define the initial page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+// https://nextjs.org/docs/app/api-reference/file-conventions/not-found
+
+import PageLayout from '~/components/layout/content/Page';
+import InternalLink from '~/components/ui/InternalLink';
+
+//----------------------------------------------------------------------------//
+
+export default function NotFound() {
+  return (
+    <PageLayout
+      className="xs:items-start flex flex-col items-center gap-6"
+      header={
+        <>
+          <InternalLink href={'/'} className="link-underline">
+            Home
+          </InternalLink>
+
+          <span className="link font-medium">404</span>
+        </>
+      }
+    >
+      <h2>Not Found</h2>
+
+      <InternalLink href="/" className="link-underline font-medium">
+        Return Home
+      </InternalLink>
+    </PageLayout>
+  );
+}
