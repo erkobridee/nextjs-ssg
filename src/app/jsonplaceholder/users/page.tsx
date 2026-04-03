c53236acf2e871069545944284fa072313df4870
erkobridee <erko.bridee@gmail.com>
2026-04-03 20:44:39 +0200

    #708 update(users jsonplaceholder page): display the user id at the list

--- src/app/jsonplaceholder/users/page.tsx
+++ src/app/jsonplaceholder/users/page.tsx
@@ -5,7 +5,7 @@ import { getUsers } from '~/api/jsonplaceholder';
 
 //----------------------------------------------------------------------------//
 
-export async function JSONPlaceholder() {
+export async function UsersJSONPlaceholder() {
   const users = await getUsers();
 
   return (
@@ -27,13 +27,21 @@ export async function JSONPlaceholder() {
     >
       <h1 className="mt-8 text-xl font-medium">Users</h1>
 
-      <ul className="list-inside list-disc">
+      <ul className="list-disc">
         {users.map(({ id, name }) => (
-          <li key={id}>{name}</li>
+          <li key={id}>
+            <div className="flex items-center gap-2">
+              <span className="tabular-nums">[{`${id}`.padStart(2, '0')}]</span>
+              <span>-</span>
+              <InternalLink href={`/jsonplaceholder/users/${id}`} className="link-underline">
+                {name}
+              </InternalLink>
+            </div>
+          </li>
         ))}
       </ul>
     </PageLayout>
   );
 }
 
-export default JSONPlaceholder;
+export default UsersJSONPlaceholder;
