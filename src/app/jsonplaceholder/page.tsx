40387b54a89692f31e9fd2b2086b77e33f1c9f05
erkobridee <erko.bridee@gmail.com>
2026-04-03 17:12:41 +0200

    #708 update(jsonplaceholder page): add the link to the users page

--- src/app/jsonplaceholder/page.tsx
+++ src/app/jsonplaceholder/page.tsx
@@ -1,12 +1,13 @@
 import PageLayout from '~/components/layout/content/Page';
 import InternalLink from '~/components/ui/InternalLink';
+import TargetBlankLink from '~/components/ui/TargetBlankLink';
 
 //----------------------------------------------------------------------------//
 
 export function JSONPlaceholder() {
   return (
     <PageLayout
-      className="xs:items-start flex flex-col items-center justify-between gap-6"
+      className="xs:items-start flex flex-col gap-6"
       header={
         <>
           <InternalLink href={'/'} className="link-underline">
@@ -21,9 +22,19 @@ export function JSONPlaceholder() {
         </>
       }
     >
-      <p>
-        <strong>TODO:</strong> define the page JSON Placeholder body
-      </p>
+      <h1 className="mt-8 text-xl">
+        <TargetBlankLink href="https://jsonplaceholder.typicode.com/" className="link font-medium">
+          JSON Placeholder
+        </TargetBlankLink>
+      </h1>
+
+      <ul className="list-inside list-disc">
+        <li>
+          <InternalLink href="/jsonplaceholder/users" className="link-underline">
+            Users
+          </InternalLink>
+        </li>
+      </ul>
     </PageLayout>
   );
 }
